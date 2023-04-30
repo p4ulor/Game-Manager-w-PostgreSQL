98e9bb69da6e31ada4558414792730ec2a244df6
p4ulor <A44873@alunos.isel.pt>
2023-04-30 10:22:23 +0100

    Update a), b), c). O a) corre bem

---
+++ d_addPlayer_disablePlayer.sql
@@ -0,0 +1,7 @@
+-- Stored procedures:
+
+-- "criar o jogador, dados os seus email, região e username" 
+
+CREATE OR REPLACE PROCEDURE createPlayer
+
+--desativar ou banir o jogador, dados os seus email, região e username
